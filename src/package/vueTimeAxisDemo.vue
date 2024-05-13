cad1a68710e0daec3349ecbad4d1f01b61d777a6
yuwb <yuwb0521@outlook.com>
2024-05-13 21:37:49 +0800

    feat:添加时间轴拉长功能

--- src/package/vueTimeAxisDemo.vue
+++ src/package/vueTimeAxisDemo.vue
@@ -7,13 +7,19 @@
     }"
   >
     <span
-      @mousedown="mouseDown($event, item)"
       class="vue-time-axis-inline"
       v-for="item in option.data"
       :key="item.id"
       :style="itemStyle(item)"
-      >{{ item.text }}</span
     >
+      <span class="item-content" @mousedown="mouseDown($event, item)">
+        {{ item.text }}
+      </span>
+      <span
+        @mousedown="mouseColResizeDown($event, item)"
+        class="col-resize"
+      ></span>
+    </span>
   </div>
 </template>
 
@@ -27,6 +33,7 @@ export default {
       id: null,
       currentItem: null,
       isMove: false,
+      isColResize: false,
       timeID: null,
       key: new Date().getTime(),
     };
@@ -44,37 +51,60 @@ export default {
       this.currentItem = item;
       this.id = item.id;
       this.isMove = true;
+      console.log(123);
+    },
+    mouseColResizeDown(e, item) {
+      this.x = e.clientX;
+      this.currentItem = item;
+      this.id = item.id;
+      this.isColResize = true;
+      console.log(456);
     },
   },
   mounted() {
     const el = document.querySelector("#vueTimeAxis" + this.key);
     el.onmousemove = (e) => {
-      if (!this.isMove || this.timeID) return;
+      if ((!this.isMove && !this.isColResize) || this.timeID) return;
       // 减少触发次数
       this.timeID = setTimeout(() => {
         this.timeID = null;
       }, 16);
       const step = this.option.width / (this.option.max - this.option.min);
       let data = (e.clientX - this.x) / step;
-      //拖拽移动限制 不能超出最小最大值
-      let spacing = this.currentItem.end - this.currentItem.start;
-      if (this.currentItem.end + data > this.option.max) {
-        this.currentItem.end = this.option.max;
-        this.currentItem.start = this.currentItem.end - spacing;
-      } else if (this.currentItem.start + data < this.option.min) {
-        this.currentItem.start = this.option.min;
-        this.currentItem.end = this.currentItem.start + spacing;
-      } else {
-        this.currentItem.start += data;
-        this.currentItem.end += data;
+      // 拖拽移动
+      if (this.isMove) {
+        //拖拽移动限制 不能超出最小最大值
+        let spacing = this.currentItem.end - this.currentItem.start;
+        if (this.currentItem.end + data > this.option.max) {
+          this.currentItem.end = this.option.max;
+          this.currentItem.start = this.currentItem.end - spacing;
+        } else if (this.currentItem.start + data < this.option.min) {
+          this.currentItem.start = this.option.min;
+          this.currentItem.end = this.currentItem.start + spacing;
+        } else {
+          this.currentItem.start += data;
+          this.currentItem.end += data;
+        }
+      } else if (this.isColResize) {
+        if (this.currentItem.end + data > this.option.max) {
+          this.currentItem.end = this.option.max;
+        } else if (this.currentItem.end + data < this.currentItem.start + 2) {
+          // 最小间距为2
+          this.currentItem.end = this.currentItem.start + 2;
+        } else {
+          this.currentItem.end += data;
+        }
       }
+      // 拖拽拉长
       this.x = e.clientX;
     };
     el.onmouseup = () => {
       this.isMove = false;
+      this.isColResize = false;
     };
     el.onmouseleave = () => {
       this.isMove = false;
+      this.isColResize = false;
     };
   },
   created() {},
@@ -109,5 +139,22 @@ export default {
   -ms-user-select: none;
   user-select: none;
   text-align: center;
+}
+.vue-time-axis-inline .item-content {
+  display: block;
+  width: 100%;
+  cursor: pointer;
+}
+.vue-time-axis-inline .col-resize {
+  position: absolute;
+  display: block;
+  right: 0;
+  top: 0;
+  text-align: center;
+  width: 10px;
+  height: 100%;
+  cursor: col-resize;
+  background-color: #3a91c7;
+  z-index: 999;
 }
 </style>
